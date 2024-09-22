05c803b4c465494b9affacef8a5270792e01b7c3
Abdul Mahl <mdu_abdul@icloud.com>
2024-09-22 20:38:23 +0200

    .

--- backend/routes/clientMessageRouter.js
+++
@@ -1,8 +0,0 @@
-const clientMessageRouter = require("express").Router();
-const clientMessageController = require("../controllers/clientMessageController");
-
-clientMessageRouter
-  .route("/")
-  .post(clientMessageController.createClientMessage);
-
-module.exports = clientMessageRouter;
